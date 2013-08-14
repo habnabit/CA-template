0cbcf9f41db05f936baeb587746e13e082df0d9e
Aaron Gallagher <habnabit@gmail.com>
2013-08-13 23:01:07 -0700

    Expand brace expansion for compatibility.

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ mkdir -p incoming
 openssl req -nodes -new -newkey rsa:2048 -keyout ca.key.pem -out incoming/ca.csr.pem
 OPENSSL_CONF=ca.cnf openssl ca -selfsign -policy policy_ca -extensions ext_ca -in incoming/ca.csr.pem -out ca.crt.pem
 
-git add ca.{crt,key}.pem certs/01.pem {index,serial}.txt
+git add ca.crt.pem ca.key.pem certs/01.pem index.txt serial.txt
 echo '---'
 echo
 echo 'Your CA certificate and key have been generated and staged. Time to `git commit`!'
